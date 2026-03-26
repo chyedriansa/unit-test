1156dda645335499249e934139b83f86c55bcd23
Cahaya Ed. <chyedriansa@gmail.com>
2026-03-26 14:54:43 +0700

    feat:add test thr for ied fitri

---
+++ test/thr.test.js
@@ -0,0 +1,10 @@
+const {hitungTHR} = require('../lib/thr-ied.js')
+
+test ('Hitung THR', function(){
+    const gaji = 15000000
+    const masaKerjaBulan = 12
+    const result = hitungTHR(gaji, masaKerjaBulan)
+    expect(result).toBe(result)
+    console.log(result)
+
+})
